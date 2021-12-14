9b611121899f818b0f803a5f67555f475dd42c38
Anika Yogi <80984914+anikayogi17@users.noreply.github.com>
2021-12-13 20:26:01 -0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
-import firebase from 'firebase';
+//import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqqpzF0tKDI8hnSCS98hLJHyHM-GcqAA4",
+  authDomain: "team-voin.firebaseapp.com",
+  databaseURL: "https://team-voin-default-rtdb.firebaseio.com",
+  projectId: "team-voin",
+  storageBucket: "team-voin.appspot.com",
+  messagingSenderId: "53871026508",
+  appId: "1:53871026508:web:0ef22aa7784d85dbaeefe9"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
